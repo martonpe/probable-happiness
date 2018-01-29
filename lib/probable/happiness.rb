3539fea5e08875b33222527bd2bac1d1ea2731ae
Peter Marton <martonpe@secretsaucepartners.com>
2018-01-29 20:34:07 +0100

    Initial commit
    Initialise project with bundle gem

---
+++ lib/probable/happiness.rb
@@ -0,0 +1,7 @@
+require "probable/happiness/version"
+
+module Probable
+  module Happiness
+    # Your code goes here...
+  end
+end
---
+++ spec/probable/happiness_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Probable::Happiness do
+  it "has a version number" do
+    expect(Probable::Happiness::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

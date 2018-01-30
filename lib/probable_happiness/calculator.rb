290104013e83d73f5e064e32e3288a34bd03babe
Peter Marton <martonpe@secretsaucepartners.com>
2018-01-30 01:44:17 +0100

    Implement Calculator

--- lib/probable_happiness/calculator.rb
+++ lib/probable_happiness/calculator.rb
@@ -1,8 +1,12 @@
 require 'json'
+require 'net/http'
 
 module ProbableHappiness
   class Calculator
+    WIKIP_API_URL = "https://www.quandl.com/api/v3/datatables/WIKI/PRICES"
+
     attr_reader :ticker, :start_date, :end_date
+    attr_accessor :values
 
     def initialize(ticker, start_date, end_date)
       @ticker = ticker
@@ -10,19 +14,46 @@ module ProbableHappiness
       @end_date = end_date
     end
 
-    def call
-      #TODO: Call the WIKIP API for ticker, start_date, end_date
-    end
-
     def total_return
+      @values = get_values if values.nil?
 
+      initial_value = values[0]
+      final_value = values[-1]
+      total_return = ( final_value - initial_value ) / initial_value * 100
+
+      total_return.round(2)
     end
 
     def max_drawdown
+      @values = get_values if values.nil?
 
+      peak = values.first
+      low = values.first
+      max_drawdown = 0
+      values.each do |value|
+        if value > peak then
+          peak = value
+          low = peak
+        elsif value < low
+          low = value
+          tmp_drawdown = peak - low
+          max_drawdown = tmp_drawdown if tmp_drawdown > max_drawdown
+        end
+      end
+      drowdown = max_drawdown / peak * 100
+
+      drowdown.round(2)
     end
 
-    private
+    def get_values
+      uri = URI(WIKIP_API_URL)
+      api_key = ENV['QUANDL_API_KEY']
+      dates = (start_date..end_date).map { |date| date.strftime('%Y-%m-%d') }
+      params = { date: dates.join(','), ticker: ticker, api_key: api_key }
+      uri.query = URI.encode_www_form(params)
+      res = Net::HTTP.get(uri)
 
+      JSON.parse(res)['datatable']['data'].map { |row| row[5] }
+    end
   end
 end
--- spec/probable_happiness/calculator_spec.rb
+++ spec/probable_happiness/calculator_spec.rb
@@ -2,6 +2,39 @@ require 'spec_helper'
 require "probable_happiness/calculator"
 
 RSpec.describe ProbableHappiness::Calculator do
+  let(:ticker) { 'AAPL' }
+  let(:start_date) { Date.new(2015-01-23) }
+  let(:end_date) { Date.new(2018-01-26) }
 
+  let(:calculator) { ProbableHappiness::Calculator.new(ticker, start_date, end_date) }
+
+  describe '#total_return' do
+    it 'handles positive changes' do
+      calculator.values = [176.19, 179.1, 190.51]
+      expect(calculator.total_return).to eq(8.13)
+    end
+
+    it 'handles negative changes' do
+      calculator.values = [200.50, 179.1, 190.19]
+      expect(calculator.total_return).to eq(-5.14)
+    end
+  end
+
+  describe '#max_drawdown' do
+    it 'handles single peaks' do
+      calculator.values = [177.0, 177.04, 174.22, 171.11, 171.1]
+      expect(calculator.max_drawdown).to eq(3.36) #(177.04-171.1)/177.04
+    end
+
+    it 'handles multiple peaks' do
+      calculator.values = [176.19, 185.1, 179.26, 178.46, 177.0, 177.04, 180.22, 174.11, 174.51]
+      expect(calculator.max_drawdown).to eq(5.94)  #(185.1-174.11)/185.1
+    end
+
+    it 'handles multiple peaks and chooses second' do
+      calculator.values = [176.19, 179.1, 179.26, 178.46, 177.0, 175.04, 180.22, 174.11, 172.51]
+      expect(calculator.max_drawdown).to eq(4.28)  #(180.22-172.51)/180.22
+    end
+  end
 end
 
